ca031463258d11bc2e5db78bec10fe8750130221
Sixian Xiong <sixianx@umd.edu>
2026-03-22 22:02:39 -0400

    Add hooked models. Add hidden states full extraction.

--- vllm/distributed/kv_transfer/kv_connector/factory.py
+++ vllm/distributed/kv_transfer/kv_connector/factory.py
@@ -156,6 +156,12 @@ KVConnectorFactory.register_connector(
 )
 
 KVConnectorFactory.register_connector(
+    "FullHiddenStatesConnector",
+    "vllm.distributed.kv_transfer.kv_connector.v1.full_hidden_states_connector",
+    "FullHiddenStatesConnector",
+)
+
+KVConnectorFactory.register_connector(
     "P2pNcclConnector",
     "vllm.distributed.kv_transfer.kv_connector.v1.p2p.p2p_nccl_connector",
     "P2pNcclConnector",

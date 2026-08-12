5c7538b29d78e1e9ca11dc7dd4683e1d511a5b9e
zhaoyboo <zhaoyboo@outlook.com>
2026-08-12 13:18:51 -0400

    Add monitored Qwen2 model variant

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -195,6 +195,7 @@ _TEXT_GENERATION_MODELS = {
     "Plamo3ForCausalLM": ("plamo3", "Plamo3ForCausalLM"),
     "QWenLMHeadModel": ("qwen", "QWenLMHeadModel"),
     "Qwen2ForCausalLM": ("qwen2", "Qwen2ForCausalLM"),
+    "Qwen2PForCausalLM": ("qwen2_p", "Qwen2PForCausalLM"),
     "Qwen2MoeForCausalLM": ("qwen2_moe", "Qwen2MoeForCausalLM"),
     "Qwen2MoePForCausalLM": ("qwen2_moe_p", "Qwen2MoePForCausalLM"),
     "Qwen2MoeCompareForCausalLM": ("qwen2_moe_compare", "Qwen2MoeCompareForCausalLM"),
